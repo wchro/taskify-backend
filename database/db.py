37f0925acd3abdb6ea156c5f7a5acf1f9fc72a5d
wchro <wchro@icloud.com>
2023-02-18 10:51:22 +0100

    Improvements

--- database/db.py
+++ database/db.py
@@ -3,9 +3,17 @@ import sqlite3
 def create_database():
     conn = sqlite3.connect("database/database.db")
     c = conn.cursor()
-    c.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password BLOB)")
+    c.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE, password BLOB)")
     conn.commit()
     conn.close()
+
+def check_if_exists(table, column, data):
+    conn = sqlite3.connect("database/database.db")
+    c = conn.cursor()
+    c.execute(f"SELECT 1 FROM {table} WHERE {column}=?", (data,))
+    result = c.fetchone()
+    conn.close()
+    return result is not None
 
 def signup(username: str, password: bytes):
     conn = sqlite3.connect("database/database.db")
--- main.py
+++ main.py
@@ -6,6 +6,9 @@ app = FastAPI()
 
 @app.post("/signup")
 async def signup(username, password):
-    password = pswrd.gen_hash(password.encode("UTF-8"))
-    db.signup(username, password)
-    return {"success": True, "msg": "You have successfully registered."}
+    if not db.check_if_exists("users", "username", username):
+        password = pswrd.gen_hash(password.encode("UTF-8"))
+        db.signup(username, password)
+        return {"success": True, "msg": "You have successfully registered."}
+    else:
+        return {"success": False, "msg": "Sorry, that username is already taken. Please try another."}
