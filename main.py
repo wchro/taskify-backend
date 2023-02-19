d6c08c7f05507234221957ee53e290e56f282e17
wchro <wchro@icloud.com>
2023-02-19 02:24:23 +0100

    Improvements

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from fastapi import FastAPI, Form
 from services import auth
 from utils import jwt_token, password as pswrd
+from database import db
 
 
 app = FastAPI()
 
 @app.post("/user/login")
 async def login(username: str = Form(), password: str = Form()):
-    if auth.login(username, password):
-        tokens = await jwt_token.get_tokens(1)
-        session_token = tokens[0]
-        access_token = tokens[1]
-        return {"success": True, "session_token": session_token, "access_token": access_token}
-    else:
+    try:
+        user = db.get_data("users", "username", username)
+        user_id = user[0]
+        password_hashed = user[2]
+        if pswrd.check_pswd(password.encode("UTF-8"), password_hashed):
+            tokens = await jwt_token.get_tokens(user_id)
+            session_token = tokens[0]
+            access_token = tokens[1]
+            return {"success": True, "session_token": session_token, "access_token": access_token}
+        else:
+            return {"success": False, "msg": "Username or password is incorrect. Please try again."}
+    except:
         return {"success": False, "msg": "Username or password is incorrect. Please try again."}
     
 @app.post("/user/register")
